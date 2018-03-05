824e65970ac8409a7bbc65f1e5d25c56fa75ff77
CBSahoo <36992486+CBSahoo@users.noreply.github.com>
2018-03-05 11:26:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -126,8 +126,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName',function(req,res){
-    
-    var articleName = req.param.articleName;
+     var articleName = req.param.articleName;
      res.send(createTemplate(articles[articleName]));
 });
 
