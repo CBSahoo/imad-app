431529ebd6a640001da64843c43cea0bc86766ad
CBSahoo <36992486+CBSahoo@users.noreply.github.com>
2018-03-06 10:55:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,53 +1,54 @@
-var button = document.getElementById('counter');
-
-button.onclick = function(){
+    var button = document.getElementById('counter');
+    
+    button.onclick = function(){
     //Create a request
     var request = new XMLHttpRequest();
     
     //Capture the request
     request.onreadystatechange = function(){
-        if(request.readyState === XMLHttpRequest.DONE){
-            //take some action
-            if(request.status === 200){
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();            
-            }
-        }
-        //Not done yet
+    if(request.readyState === XMLHttpRequest.DONE){
+    //take some action
+    if(request.status === 200){
+    var counter = request.responseText;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();            
+    }
+    }
+    //Not done yet
     };
     
     //Make request
     request.open('GET','http://bhanusahoo08.imad.hasura-app.io/counter',true);
     request.send(null);
-};
-
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-var submit = document.getElementById('submit_btn');
-submit.onclick = function(){
+    };
+    
+    
+    var submit = document.getElementById('submit_btn');
+    submit.onclick = function(){
     //Create request object
-     var request = new XMLHttpRequest();
+    var request = new XMLHttpRequest();
     
     //Capture the request
     request.onreadystatechange = function(){
-        if(request.readyState === XMLHttpRequest.DONE){
-            //take some action
-            if(request.status === 200){
-               var names = request.responseText;
-               names = JSON.parse(names);
-  var list = '';
-  for (var i=0;i<names.length;i++){
-      list += '<li>' + names[i] + '</li>';
-  }
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = list;        
-            }
-        }
-        //Not done yet
+    if(request.readyState === XMLHttpRequest.DONE){
+    //take some action
+    if(request.status === 200){
+    var names = request.responseText;
+    names = JSON.parse(names);
+    var list = '';
+    for (var i=0;i<names.length;i++){
+    list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;        
+    }
+    }
+    //Not done yet
     };
     
     //Make request
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET','http://bhanusahoo08.imad.hasura-app.io/submit-name?name=' +name,true);
     request.send(null);
-};
+    };
