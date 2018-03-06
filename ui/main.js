4758da4d0e294c58b05a4b7c0d40d7ad5cd4953a
CBSahoo <36992486+CBSahoo@users.noreply.github.com>
2018-03-06 10:32:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,15 +22,15 @@ button.onclick = function(){
     request.send(null);
 };
 
-var nameInput = document.getElementByID('name');
+var nameInput = document.getElementById('name');
 var name = nameInput.value;
-var submit = document.getElementByID('submit_btn');
-submit.onclick =  fucntion(){
-  var names = ['name1','name2','name3'];
-  var list='';
-  for (var i= 0;i<names.length;i++){
-      list += '<li>' + names[i] + '</li>'';
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+  //Capture a list of names and render it as a list
+  var names = ['name1','name2','name3','name4'];
+  var list = '';
+  for (var i=0;i<names.length;i++){
+      list += '<li>' + names[i] + '</li>'
   }
   var ul = document.getElementById('namelist');
-  ul.innerHTML = list;
 };
