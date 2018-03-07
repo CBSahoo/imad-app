d91b8820a6c96a9ea10dd06faf0768a5cff7cbda
CBSahoo <36992486+CBSahoo@users.noreply.github.com>
2018-03-07 11:42:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -135,7 +135,7 @@ app.post('/login',function(req,res){
                 res.status(403).send('username/password is invalid');
             }else{
              var dbString = result.rows[0].password;
-             var salt = dbString.split('$2')[2];
+             var salt = dbString.split('$')[2];
              var hashedPassword = hash(password,salt);
              if(hashedPassword === dbString){
                  res.send('Successfully logged in!');
